12cf524c6272afcfca3e5129fecb312540892370
miverabusiness <miverabusiness@gmail.com>
2023-02-11 23:39:15 +0100

    Basic structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import { Nav, Navbar, Image } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <Navbar variant='dark' sticky='top' expand='lg' collapseOnSelect>
+          <Navbar.Brand><a href='/' className='navbar-logo'>MAKINETA FC</a></Navbar.Brand>
+          
+          <Navbar.Toggle />
+          <Navbar.Collapse className='justify-content-end'>
+            <Nav>
+              <Nav.Link hfer='/'>HOME</Nav.Link>
+              <Nav.Link hfer='#'>SQUAD</Nav.Link>
+              <Nav.Link hfer='#'>CONTACT</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+
+        </Navbar>
+      </div>
+
+      <div className="home">
+        Texto de prueba
+      </div>
     </div>
   );
 }
